d013d0a66c0b97d57bccf72efa724a943647a844
ramiro-c <rami992009@gmail.com>
2020-03-28 19:23:16 -0300

    Consultar la api y guardar la frase

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+  const [frase, guardarFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  };
+
   return (
-    <h1>Breaking Bad</h1>
+    <Contenedor>
+    <Boton
+      onClick={consultarAPI}
+    >
+      Obtener Frase
+    </Boton>
+    </Contenedor>
   );
 }
 
