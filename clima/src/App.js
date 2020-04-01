0de6502c92fc1da9475923a5b9803cca857b6b23
ramiro-c <rami992009@gmail.com>
2020-04-01 05:37:50 -0300

    Header terminado, avances en el Formulario, mensaje de error y validacion de los campos terminados

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div></div>
+    <Fragment>
+      <Header
+        titulo="Clima React App"
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
