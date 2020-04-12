0847b5ceb1361a947a797ec02f03d5c87596f96e
Ramiro Cerdá <rami992009@gmail.com>
2020-04-12 02:55:14 -0300

    Avance SelectNoticias

--- noticias/src/components/Formulario.js
+++ noticias/src/components/Formulario.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Formulario.module.css";
+import useSelect from "../hooks/useSelect";
 
 const Formulario = () => {
+  const CATEGORIAS = [
+    { value: "general", label: "General" },
+    { value: "business", label: "Negocios" },
+    { value: "entertainment", label: "Entretenimiento" },
+    { value: "health", label: "Salud" },
+    { value: "science", label: "Ciencia" },
+    { value: "sports", label: "Deportes" },
+    { value: "technology", label: "Tecnologia" },
+  ];
+  const [categoria, SelectNoticias] = useSelect("technology", CATEGORIAS);
+
   return (
     <div className={`$(styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
         <form>
           <h2 className={styles.heading}>Encuentra Noticias por Categoria</h2>
+          <SelectNoticias />
           <div className="input-field col s12">
             <input
               type="submit"
