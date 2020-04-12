f3d46f2e1049c21ce3c297f8bb9d34a984b847c9
Ramiro Cerdá <rami992009@gmail.com>
2020-04-12 04:42:45 -0300

    Listado de las noticias

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
+import ListadoNoticias from "./components/ListadoNoticias";
 
 function App() {
   const [categoria, guardarCategoria] = useState("");
@@ -26,6 +27,7 @@ function App() {
       <Header titulo="Buscador de Noticias" />
       <div className="container white">
         <Formulario guardarCategoria={guardarCategoria} />
+        <ListadoNoticias noticias={noticias} />
       </div>
     </Fragment>
   );
