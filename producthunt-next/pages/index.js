3bb7339d7bcd8efc5601f67975e3f756d371562d
Ramiro Cerdá <rami992009@gmail.com>
2020-05-03 13:07:28 -0300

    consultar bbdd de firebase

--- producthunt-next/pages/index.js
+++ producthunt-next/pages/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Layout from "../components/layout/Layout";
+import { FirebaseContext } from "../firebase";
 
 const Home = () => {
+  const [productos, guardarProductos] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(handleSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+
+  function handleSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    guardarProductos(productos);
+  }
   return (
     <Layout>
       <h1>Inicio</h1>
