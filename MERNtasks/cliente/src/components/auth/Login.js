982f95df61218ad08c4e84cbcc3beccd3b7f86e5
Ramiro Cerdá <rami992009@gmail.com>
2020-05-06 16:52:27 -0300

    login form

--- MERNtasks/cliente/src/components/auth/Login.js
+++ MERNtasks/cliente/src/components/auth/Login.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const [user, setUser] = useState({ email: "", password: "" });
+  const { email, password } = user;
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Login</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Login"
+            />
+          </div>
+        </form>
+        <Link to={"/new-account"} className="enlace-cuenta">
+          Don't you have an account? Sign in now
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
