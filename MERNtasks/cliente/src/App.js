414586034be617972eceaf3959fd736bbf731477
Ramiro Cerdá <rami992009@gmail.com>
2020-05-06 13:07:13 -0300

    first routes

--- MERNtasks/cliente/src/App.js
+++ MERNtasks/cliente/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Login from "./components/auth/Login";
+import NewAccount from "./components/auth/NewAccount";
+import Projects from "./components/projects/Projects";
 
 function App() {
-  return <h1>MERN Tasks</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/new-account" component={NewAccount} />
+        <Route exact path="/projects" component={Projects} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
