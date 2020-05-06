d6956d8f2b3d85da90b9d5bf1fcc7d055638f15f
Ramiro Cerdá <rami992009@gmail.com>
2020-05-06 17:56:05 -0300

    sign in form

--- MERNtasks/cliente/src/components/auth/NewAccount.js
+++ MERNtasks/cliente/src/components/auth/NewAccount.js
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NewAccount = () => {
-  return <h1>NewAccount</h1>;
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm: "",
+  });
+  const { name, email, password, confirm } = user;
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Register</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Your name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="confirm">Repeat password</label>
+            <input
+              type="password"
+              id="confirm"
+              name="confirm"
+              value={confirm}
+              placeholder="Repeat your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Register"
+            />
+          </div>
+        </form>
+        <Link to={"/"} className="enlace-cuenta">
+          Do you have an account? Login now
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NewAccount;
